1ca3bea39cee42c05716bc22c678b6ae4b8482fd
minjeong65 <aplus930@gmail.com>
2021-11-10 14:20:31 +0900

    Update 구현

--- book/urls.py
+++ book/urls.py
@@ -4,7 +4,6 @@ from . import views
 app_name = 'book' #앱 마다 별칭이 겹칠 수 있으므로 앱 이름으로 구분
 urlpatterns = [
     path('', views.index, name = 'index'),
-    path('detail/<pk>',views.detail, name='detail'),
     path('create', views.create, name = "create"),
     path('update/<pk>', views.update, name='update'),
     path('delete/<pk>', views.delete, name='delete'),
--- book/views.py
+++ book/views.py
@@ -11,14 +11,6 @@ def index(request):
     return render(request, 'book/index.html', context)
 
 
-def detail(request,pk):
-    b= Book.objects.get(id=pk)
-    context={
-        'book' : b,
-    }
-    return render(request, 'book/detail.html', context)
-
-
 def create(request):
     if request.method == "POST":
         sn=request.POST.get("sname")
@@ -27,10 +19,28 @@ def create(request):
         return redirect("book:index")
     return render(request, 'book/create.html')
 
+
 def delete(request, pk):
-    b= Book.objects.get(id=pk)
+    b = Book.objects.get(id=pk)
     b.delete()
     return redirect("book:index")
 
+
 def update(request,pk):
-    return redirect("book:index")
+    b = Book.objects.get(pk=pk)
+
+    if request.method == "POST":
+        print(1)
+        sn = request.POST.get("site_name")
+        su = request.POST.get("site_url")
+        b.site_name = sn 
+        b.site_url = su
+        b.save()    
+        return redirect('book:index')
+    
+    context = {
+        'b': b 
+    }
+    return render(request, 'book/update.html', context)
+
+
